7d7e3956985a603549d1816c1602b36c5831cc90
thvitti <thvittix@gmail.com>
2021-05-31 14:59:40 -0300

    component Tasks created

---
+++ src/components/tasks/Tasks.js
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import List from './list/List';
+
+import CreateTask from './create_tasks/CreateTasks';
+import Button from 'react-bootstrap/Button';
+
+
+class Tasks extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        tasks: []
+      };
+      this.loadTasks = this.loadTasks.bind(this);
+  }
+
+  async loadTasks() {
+    let response = await fetch(`/tasks`);
+    const tasks = await response.json();
+    this.setState({ tasks: tasks });
+  }
+
+  async deleteAllTasks(){
+      if( window.confirm('This action will remove all tasks. Continue?') ){
+        await fetch(`/tasks/destroy_all`, {method: 'DELETE'})
+        this.loadTasks();
+      }
+  }
+
+  componentDidMount() {
+    this.loadTasks();
+  }
+
+
+
+  render() {
+    return(
+      <Row>
+           <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+             <p className="title">To-do</p>
+             <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter( (task) => task.done !== true)}/>
+             <CreateTask loadTasks={this.loadTasks}/>
+           </Col>
+           <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+             <p className="title">Done</p>
+             <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter( (task) =>  task.done === true )}/>
+             <Button variant="red" className="float-right remove_tasks_btn" onClick={() => this.deleteAllTasks() } >Remove all tasks</Button>
+           </Col>
+         </Row>
+    );
+  }
+}
+
+export default Tasks;
