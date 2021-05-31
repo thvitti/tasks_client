5e68e02dd648d9d54d28a0d7fcbcdbb327cb777b
thvitti <thvittix@gmail.com>
2021-05-31 15:01:01 -0300

    components List and Create Tasks added

---
+++ src/components/tasks/create_tasks/CreateTasks.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
+
+function CreateTask(props) {
+    const [title, setTitle] = useState('');
+    const [show, setShow] = useState('');
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (async (event) => {
+
+      const form = event.currentTarget;
+
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (form.checkValidity() === false) {
+          setValidated(true);
+          return;
+      }
+
+
+      let payload = getData()
+
+      await fetch(`/tasks`, payload)
+       .catch(error => console.log(error));
+       clearForm()
+       props.loadTasks();
+
+      });
+
+      const handleClose = (event) => {
+       clearForm()
+      };
+
+      const clearForm = () => {
+       setShow(false)
+       setTitle('')
+      }
+
+      const getData = () => {
+        let task    = { title: title, done: false }
+        let body    = JSON.stringify(task)
+        let method  = 'POST'
+        let headers = new Headers();
+
+        headers.append('Content-Type', 'application/json');
+        headers.append('Accept', 'application/json');
+
+        return {method, headers, body}
+      }
+
+
+ return (
+   <div>
+     <Button onClick={e => setShow(true)}
+             variant="dark"
+             className="float-right create_task_btn">+ Tasks</Button>
+
+     <Modal show={show || false}
+            onHide={e => setShow(false)}
+            animation={false} >
+       <Form noValidate
+             validated={validated}
+             onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>New Task</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form.Row>
+            <Form.Group as={Col} md="6" controlId="validationCustom">
+              <Form.Label>New Task</Form.Label>
+              <Form.Control type="text"
+                           placeholder="Enter with your task..."
+                           value={title || ''}
+                           onChange={e => setTitle(e.target.value)}
+                           required
+                           autoComplete="off"
+                           />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+        </Modal.Body>
+
+        <Modal.Footer>
+         <Button variant="secondary" onClick={handleClose}>
+           Close
+         </Button>
+         <Button variant="dark" type="submit">
+           Create
+         </Button>
+        </Modal.Footer>
+       </Form>
+    </Modal>
+
+   </div>
+ );
+}
+
+export default CreateTask;
---
+++ src/components/tasks/list/List.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import Card from 'react-bootstrap/Card';
+import Table from 'react-bootstrap/Table';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class List extends Component {
+
+  async deleteTask(task) {
+      if ( window.confirm(`Are you sure you want to delete: "${task.title}" ?`) ){
+        await fetch(`/tasks/${task.id}`, {method: 'DELETE'} );
+          this.props.loadTasks();
+      }
+  }
+
+  async checkTask(task) {
+
+       let method = 'PUT';
+
+       let headers = {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       };
+
+       let payload = {'task': {'done': 'true'}}
+
+       let body = JSON.stringify(payload)
+
+       let data = { method, headers, body }
+
+       await fetch(`/tasks/${task.id}`, data);
+
+       this.props.loadTasks();
+  }
+
+
+  render() {
+    return(
+      <div>
+        <Card>
+          <Card.Body>
+            <Table responsive>
+              <tbody>
+                {this.props.tasks.map( (task, index) => {
+                  return <tr key={task.id}>
+                    <td className="col-md-10">{task.title}
+                    </td>
+                    <td>
+                      {
+                        task.done === false
+                        ? <a className="delete" href="/#">
+                            <FontAwesomeIcon icon="check-circle" size="lg" onClick={() => this.checkTask(task)}/>
+                          </a>
+                        : null
+                      }
+                    </td>
+                    <td>
+                      <a className="delete" href="/#">
+                        <FontAwesomeIcon icon="trash-alt" onClick={ () => this.deleteTask(task) }/>
+                      </a>
+                    </td>
+                  </tr>;
+                } )}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
+}
+export default List;
